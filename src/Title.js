488461e387d0fc5eb5f960af91d3c68b347a4ae7
Ivy Nzioka <nziokaivy@gmail.com>
2019-10-24 20:27:26 +0200

    feature(icon): Changing icon as weather changes - Change icon of the weather as the weather changes and the background image - [Finishes #169230868]

--- src/Title.js
+++ src/Title.js
@@ -1,9 +1,24 @@
 import React from "react";
-import './App.css';
-
+import "./App.css";
+import iconsObject from "./icons";
 
 class Titles extends React.Component {
+    state = {
+        icon: "Clouds",
+    }
+
     render () {
+      
+
+
+        // console.log('our-obj',Thunderstorm)
+        
+        // console.log(this.props.iconType)
+
+        // const iconItem = icons.filter(icon => icon.main === this.props.iconType)[0];
+        // const {icon} = iconItem;
+console.log(iconsObject[this.state.icon]);
+
         return (
             <div class="row">
                 <div class="column left">
@@ -11,7 +26,7 @@ class Titles extends React.Component {
                         <div class="grid">
                             <div class="span-row-2"><h2>08&#176;</h2></div>
                             <div><p id="city">Kigali</p></div>
-                            <div id="icon"><h1>&#127783;</h1></div>
+                            <div id="icon"><img alt="" src = { `${iconsObject[this.state.icon]}` }/></div>
                             <div><p id="date">06:09 - Sunday, 19 Oct '19</p></div>
                             <div><p>Rainy</p></div>
                         </div>
---
+++ src/icons.js
@@ -0,0 +1,30 @@
+import rainIcon from './images/rain.png';
+import sunIcon from './images/sun.png';
+import snowIcon from './images/snow.png';
+import fogIcon from './images/fog.png';
+import hazeIcon from './images/haze.png';
+import dustIcon from './images/dust.png';
+import tornadoIcon  from './images/tornado.png';
+import cloudIcon from './images/cloudy.png';
+
+
+const changingIcons = {
+     Thunderstorm: rainIcon,
+     Rain: rainIcon,
+     Drizzle: rainIcon,
+     Snow: snowIcon,
+     Mist: fogIcon,
+     Smoke: fogIcon,
+     Haze: hazeIcon,
+     Dust: dustIcon,
+     Sand: dustIcon,
+     Fog: fogIcon,
+     Ash: fogIcon,
+     Squall: fogIcon,
+     Tornado: tornadoIcon,
+     Foggy: fogIcon,
+     Clear: sunIcon,
+     Clouds: cloudIcon,
+};
+
+export default changingIcons;
