c90f7646ed9092e746b6915cd53a0cbc93741fbc
hezzie <56603461+hezzie@users.noreply.github.com>
2019-10-25 13:44:18 +0200

    ch(responsiveness):make app responsiveness (#7)
    Finishes #169362934

--- src/components/Title.js
+++ src/components/Title.js
@@ -9,7 +9,7 @@ class Titles extends React.Component {
                 <h6>Odd Weather</h6>
                 {!error && 
                     <div class="weatherGrid">
-                        <div class="degree-span-row-2"><h2>{convertDegrees(data.list === undefined ? "": data.list[0].main.temp)}&#176;</h2></div>
+                        <div class="degree-span-row-2"><h2>{convertDegrees(data.list === undefined ? "": data.list[0].main.temp)}&#176;C</h2></div>
                         <div><p id="city">{data.list === undefined ? "" : data.city.name}</p></div>
                         <div id="icon"><img alt="" src = { `http://openweathermap.org/img/w/${data.list === undefined ? "" : data.list[0].weather[0].icon}.png`}/></div>
                         <div><p id="date">{data.list === undefined ? "" : data.list[0].dt_txt}</p></div>
