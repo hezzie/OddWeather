43da6da6367b93455360ab4383af119e0147dfb6
NiyoEric <eric.niyongabo@andela.com>
2019-10-27 17:23:27 +0200

    ch(configuration): edit path - [Finishes #169383181]

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,17 @@
 import React from "react";
-import convertDegrees from '../helpers';
+import convertDegrees from '../helpers/convert';
 
 class Titles extends React.Component {
     render () {
-        const { error, data } = this.props.titleState;      
+        const { error, data } = this.props.titleState;   
+        console.log(data);
+           
         return (
             <div>
                 <h6>Odd Weather</h6>
                 {!error && 
-                    <div class="weatherGrid">
-                        <div class="degree-span-row-2"><h2>{convertDegrees(data.list === undefined ? "": data.list[0].main.temp)}&#176;C</h2></div>
+                    <div className="weatherGrid">
+                        <div className="degree-span-row-2"><h2>{convertDegrees(data.list === undefined ? "": data.list[0].main.temp)}&#176;C</h2></div>
                         <div><p id="city">{data.list === undefined ? "" : data.city.name}</p></div>
                         <div id="icon"><img alt="" src = { `http://openweathermap.org/img/w/${data.list === undefined ? "" : data.list[0].weather[0].icon}.png`}/></div>
                         <div><p id="date">{data.list === undefined ? "" : data.list[0].dt_txt}</p></div>
